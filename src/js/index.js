255300688511b24730413484dc28cf3024aff16d
afrid <afridahamed14@gmail.com>
2024-01-04 10:20:23 +0530

    Change in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -9,41 +9,44 @@ let containerDiv = document.querySelector(".container"); // Container div
 // Decide an index for the profile to be displayed from DB.JSON
 let index = 0;
 
-// IIFE for displaying a profile initially
-(() => {
-  containerDiv.innerHTML += render(results[index]);
-})();
+containerDiv.innerHTML += render(results[index]);
+setUpEvents();
 
-// Select all elements from DOM
-let forwardBtn = document.querySelector("#icon-forward"); // Forward button
-let backBtn = document.querySelector("#icon-back"); // Back button
-let supriseMeBtn = document.querySelector("#surprise"); // Surprise Button
+function setUpEvents() {
+  // Select all elements from DOM
+  let forwardBtn = document.querySelector("#icon-forward"); // Forward button
+  let backBtn = document.querySelector("#icon-back"); // Back button
+  let supriseMeBtn = document.querySelector("#surprise"); // Surprise Button
 
-// When forward button is clicked, need to show the next profile in the array.
-forwardBtn.addEventListener("click", (evt) => {
-  index++;
-  // If the index goes beyond the length of array, display the first profile in the array
-  if (index > results.length - 1) {
-    index = 0;
-  }
-  containerDiv.innerHTML = "";
-  containerDiv.innerHTML += render(results[index]);
-});
+  // When forward button is clicked, need to show the next profile in the array.
+  forwardBtn.addEventListener("click", (evt) => {
+    index++;
+    // If the index goes beyond the length of array, display the first profile in the array
+    if (index > results.length - 1) {
+      index = 0;
+    }
+    containerDiv.innerHTML = "";
+    containerDiv.innerHTML += render(results[index]);
+    setUpEvents();
+  });
 
-// When backward button is clicked, need to show the previous profile in the array.
-backBtn.addEventListener("click", (evt) => {
-  index--;
-  // If the index goes less than 0, display the last profile in the array
-  if (index < 0) {
-    index = results.length - 1;
-  }
-  containerDiv.innerHTML = "";
-  containerDiv.innerHTML += render(results[index]);
-});
+  // When backward button is clicked, need to show the previous profile in the array.
+  backBtn.addEventListener("click", (evt) => {
+    index--;
+    // If the index goes less than 0, display the last profile in the array
+    if (index < 0) {
+      index = results.length - 1;
+    }
+    containerDiv.innerHTML = "";
+    containerDiv.innerHTML += render(results[index]);
+    setUpEvents();
+  });
 
-// When surprise button is clicked, need to show the random profile in the array.
-supriseMeBtn.addEventListener("click", (evt) => {
-  index = Math.floor(Math.random() * (results.length - 1 - 0)) + 0;
-  containerDiv.innerHTML = "";
-  containerDiv.innerHTML += render(results[index]);
-});
+  // When surprise button is clicked, need to show the random profile in the array.
+  supriseMeBtn.addEventListener("click", (evt) => {
+    index = Math.floor(Math.random() * (results.length - 1 - 0)) + 0;
+    containerDiv.innerHTML = "";
+    containerDiv.innerHTML += render(results[index]);
+    setUpEvents();
+  });
+}
